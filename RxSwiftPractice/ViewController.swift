0c01c63835b8443bb364e9b5a75a938902d8ddc2
Green <handkid1986@gmail.com>
2019-04-22 14:57:07 +0800

    Step by Step to authorized.

--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -14,13 +14,14 @@ import SnapKit
 class ViewController: UIViewController {
 
     let bag = DisposeBag()
-    let viewModel = HomeViewModel()
+    let viewModel = SimpleRxViewModel()
+    var authrorizedRequest: Observable<Never>!
     let authorizedButton = UIButton(type: .custom)
+    let fetchPlaylistButton = UIButton(type: .custom)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         
         authorizedButton.setTitle("Authorize", for: .normal)
         authorizedButton.setTitleColor(.black, for: .normal)
@@ -31,66 +32,32 @@ class ViewController: UIViewController {
             make.leading.equalTo(10)
         }
         
-//        authorizedButton.rx
-//            .controlEvent(.touchUpInside)
-//            .asDriver()
-//            .drive(viewModel.startTrigger)
-//            .disposed(by: bag)
-    
-        let authrorizedRequest = authorizedButton.rx
-            .controlEvent(.touchUpInside)
-            .asObservable()
-            .flatMap { _ in
-                APIEngine.shared.authorizeKKBOX()
-            }
-            .share(replay: 1, scope: .whileConnected)
-
-//        let loggedIn = authrorizedRequest.map { _ in
-//                !APIEngine.shared.loggedin
-//            }
-//            .asDriver(onErrorJustReturn: false)
-//
-//
-//        loggedIn.map {
-//                $0 ? "loggedin" : "authorized"
-//            }
-//            .drive(authorizedButton.rx.title())
-//            .disposed(by: bag)
-//
-//        loggedIn
-//            .drive(authorizedButton.rx.isEnabled)
-//            .disposed(by: bag)
-        
-        
-        authrorizedRequest
-            .asObservable()
-            .do(onNext: { _ in
-                self.authorizedButton.setTitle(APIEngine.shared.loggedin ? "loggedin" : "Authorized" , for: .normal)
-            })
-            .flatMap { _ in
-                APIEngine.shared.fetchFeaturedPlaylist()
-            }
-            .map { (arg0) -> [Playlist] in
-                let (playlists, _) = arg0
-                return playlists
-            }
-            .bind(to: viewModel.result)
+        authorizedButton.rx.controlEvent(.touchUpInside)
+            .bind(onNext: self.simpleAuthorize)
             .disposed(by: bag)
-        
-        
-        
-//        let fetchPlaylistRequest = authrorizedRequest.asObservable().flatMap { _ in
-//            APIEngine.shared.fetchFeaturedPlaylist()
-//        }
-//
-//        fetchPlaylistRequest.map { (arg0) -> [Playlist] in
-//            let (playlists, _) = arg0
-//            return playlists
-//        }
-//        .bind(to: viewModel.result)
-//        .disposed(by: bag)
+
+        fetchPlaylistButton.setTitle("Playlists", for: .normal)
+        fetchPlaylistButton.setTitleColor(.black, for: .normal)
+        view.addSubview(fetchPlaylistButton)
+        fetchPlaylistButton.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: 100, height: 20))
+            make.top.equalTo(60)
+            make.leading.equalTo(10)
+        }
+        fetchPlaylistButton.isHidden = true
     }
 
+    func simpleAuthorize() {
+        viewModel.authorize()
+            .observeOn(MainScheduler.instance)
+            .subscribe(onCompleted: {
+                self.authorizedButton.isHidden = true
+                self.fetchPlaylistButton.isHidden = false
+            }, onError: { _ in
+                
+            })
+            .disposed(by: bag)
+    }
 
     func fetchPlaylists() {
         APIEngine.shared.fetchFeaturedPlaylist(page: 0)
