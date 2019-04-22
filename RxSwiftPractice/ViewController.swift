fcb183a8652ce67ed8064a9e0ab63181c0dd6ffa
Green <handkid1986@gmail.com>
2019-04-22 15:44:00 +0800

    Bridge playlists to ViewController, use data building to trigger reload.

--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -19,9 +19,17 @@ class ViewController: UIViewController {
     let authorizedButton = UIButton(type: .custom)
     let fetchPlaylistButton = UIButton(type: .custom)
     
+    let tableview = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        configurationUIs()
+        setupBuildings()
+    }
+
+    func configurationUIs() {
         
         authorizedButton.setTitle("Authorize", for: .normal)
         authorizedButton.setTitleColor(.black, for: .normal)
@@ -33,9 +41,9 @@ class ViewController: UIViewController {
         }
         
         authorizedButton.rx.controlEvent(.touchUpInside)
-            .bind(onNext: self.viewModel.startAuthorizing)
+            .bind(onNext: viewModel.startAuthorizing)
             .disposed(by: bag)
-
+        
         fetchPlaylistButton.setTitle("Playlists", for: .normal)
         fetchPlaylistButton.setTitleColor(.black, for: .normal)
         view.addSubview(fetchPlaylistButton)
@@ -46,7 +54,19 @@ class ViewController: UIViewController {
         }
         fetchPlaylistButton.isHidden = true
         
+        fetchPlaylistButton.rx.controlEvent(.touchUpInside)
+            .bind(onNext: viewModel.fetchMore)
+            .disposed(by: bag)
         
+        tableview.dataSource = self
+        view.addSubview(tableview)
+        tableview.snp.makeConstraints { make in
+            make.top.equalTo(fetchPlaylistButton.snp.bottom)
+            make.leading.trailing.bottom.equalTo(view)
+        }
+    }
+    
+    func setupBuildings() {
         viewModel.loggedIn
             .asObservable()
             .observeOn(MainScheduler.instance)
@@ -58,17 +78,42 @@ class ViewController: UIViewController {
                 self.fetchPlaylistButton.isHidden = !loggedIn
             }
             .disposed(by: bag)
-    }
-
-
-    func fetchPlaylists() {
-        APIEngine.shared.fetchFeaturedPlaylist(page: 0)
+        
+        viewModel.playlists
+            .asObservable()
             .observeOn(MainScheduler.instance)
-            .subscribe(onSuccess: { tuple in
-                print(tuple.0)
+            .subscribe({ event in
+                switch event {
+                case .next(_):
+                    self.tableview.reloadData()
+                default:
+                    print("event: \(event)")
+                }
             })
             .disposed(by: bag)
+        
+    }
+    
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.playlists.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        
+        guard let aCell = cell else { fatalError() }
+        
+        let playlist = viewModel.playlists.value[indexPath.row]
+        aCell.textLabel?.text = playlist.title
+        return aCell
+        
     }
 }
 
