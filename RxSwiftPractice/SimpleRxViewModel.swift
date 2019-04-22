9fa1bc174dcbf43d883e4fa818eeaa50697dbb39
Green <handkid1986@gmail.com>
2019-04-22 15:43:03 +0800

    Update the func fetchPlaylist, using page to do the element and add filter.

--- RxSwiftPractice/SimpleRxViewModel.swift
+++ RxSwiftPractice/SimpleRxViewModel.swift
@@ -14,6 +14,7 @@ class SimpleRxViewModel {
     private let bag = DisposeBag()
     private let api = APIEngine.shared
     private(set) var page = 0
+    private(set) var hasMore = true
     
     let playlists = BehaviorRelay<[Playlist]>(value: [])
     let loggedIn = BehaviorRelay<Bool>(value: false)
@@ -37,20 +38,36 @@ class SimpleRxViewModel {
     }
     
     func fetchMore() {
-        
+        fetchPlaylist()
+            .filter {
+                $0.count > 0
+            }
+            .subscribe(onSuccess: {
+                let newPlaylists = self.playlists.value + $0
+                self.playlists.accept(newPlaylists)
+            })
+            .disposed(by: bag)
     }
     
     private func fetchPlaylist() -> Single<[Playlist]> {
-        return api.fetchFeaturedPlaylist(page: page)
-            .do(onSuccess: { (argument) in
+        return Observable.of(page).filter { _ in
+                self.hasMore == true
+            }
+            .flatMap { page -> Single<([Playlist], Bool)> in
+                self.api.fetchFeaturedPlaylist(page: page)
+            }
+            .do(onNext: { (argument) in
                 let (_, hasMore) = argument
                 if hasMore {
                     self.page += 1
                 }
+                self.hasMore = hasMore
             })
+            .asSingle()
             .flatMap { (argument) -> Single<[Playlist]> in
-            let (playlists, _) = argument
-            return Single.just(playlists)
-        }
+                let (playlists, _) = argument
+                return Single.just(playlists)
+            }
+            .catchErrorJustReturn([])
     }
 }
